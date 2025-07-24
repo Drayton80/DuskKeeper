c87cc3f24559f7c624faa1d39f92396a4f314747
Drayton80 <drayton80@hotmail.com>
2025-07-24 19:39:14 -0300

    setup: Prisma and env updates to match the database

--- server/src/database/client.ts
+++ server/src/database/client.ts
@@ -1,5 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
-const client = new PrismaClient();
+declare global {
+  var prisma: PrismaClient | undefined;
+}
 
-export { client as dbClient };
+export const prisma = global.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  global.prisma = prisma;
+}
